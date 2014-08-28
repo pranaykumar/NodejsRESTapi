27201911526b1c7cf0476b48e3027eb171fc8620
pranaykumar <pranay.mishra@gmail.com>
2014-08-28 23:35:35 +0530

    1. Adding latest for body-parser and async npm pkgs 2. Added intial code for generating nested JSON array for profiles with streams 3. sample code in server.js to demo async lib usage

--- expressServer.js
+++ expressServer.js
@@ -3,6 +3,7 @@ var express = require('express');
 var mysql = require('mysql');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var async = require('async');
 
 /* New Application ENV OBJ */
 var app = express();
@@ -48,8 +49,8 @@ app.get('/api/providers/:id', function(req, res) {
 			+ connection.escape(req.params.id), function(err, rows, fields) {
 		if (err){
 			console.log(err);
-//			results = [{"Error":"No Record found for the given Provider ID"}];
-//			res.send(results);
+// results = [{"Error":"No Record found for the given Provider ID"}];
+// res.send(results);
 			}
 		results = rows;
 		res.type('application/json');
@@ -63,11 +64,50 @@ app.get('/api/providers/:id/profiles', function(req, res) {
 	connection.query('select pf.profile_id,pf.name,pf.type,pf.private,pf.deinterlace_input,pf.frame_rate,pf.mezzanine_multipass_encoding \
 					  from providers pv, profile pf, provider_profile_map m \
 					  where pv.provider_id = m.provider_id and pf.profile_id = m.profile_id \
-					  and pv.provider_id = '+ connection.escape(req.params.id), function(err, rows, fields) {
+					  and pv.provider_id = '+ connection.escape(req.params.id), function(err, profile_rows, fields) {
 		if (err)
 			throw err;
-		results = rows;
 		
+					  var getStreams = function (profile_obj,doneCallback){
+							
+						  stream_qry = 'select s.stream_id,s.name,s.muxing_format,s.profile,s.audio_sample_rate,s.audio_bitrate,\
+							  s.video_bitrate,s.video_width,s.keyframe_interval_sec,s.watermark,s.multipass_encoding,\
+							  s.segment_length_sec,s.encrypt,s.key_rotation_period,s.video_encryption_level\
+							  from stream s, profile_stream_map ps where s.stream_id = ps.stream_id\
+							  and ps.profile_id ='+connection.escape(profile_obj.profile_id);
+						  
+						  connection.query(stream_qry,function(err, stream_rows, fields){
+							  if(err)
+								  throw err;
+							  
+							  console.log(stream_rows);
+							// Nothing went wrong, so callback with a null
+								// error.
+								  return doneCallback(null);	
+						  });
+							
+						};
+						
+						async.each(profile_rows,getStreams,function(err){
+							console.log('Finished!');
+						});
+
+		
+// // loop through the profile array returned and get streams for each
+// // profile ID
+// for ( var i = 0, len = results.length; i < len; i++) {
+//			
+ 
+//
+//				
+// this.results[this.i].streams = stream_rows;
+// console.log(stream_rows);
+// console.log(this.results);
+//
+// }.bind( {i: i,results: results} ));
+//			
+// }
+		results = profile_rows;
 		res.type('application/json');
 		res.send(results);
 	});
@@ -76,28 +116,27 @@ app.get('/api/providers/:id/profiles', function(req, res) {
 /* Add New Provider */
 app.post('/api/providers/', function(req, res) {
 	var qry = 'insert into providers (id,provider_id,name,email) values (NULL, uuid(),'+ connection.escape(req.body.name) + ',' + connection.escape(req.body.email) + ')';
-	console.log(qry);
 	connection.query(qry, function(err, result) {
 				if (err)
 					throw err;
 				if (result)
 					res.type('application/json');
 				res.send([ {
-					"provider added" : 1
+					"msg" : "Provider added."
 				} ]);
 			});
 });
 
 /* Update Provider */
 app.put('/api/providers/:id', function(req, res) {
-	connection.query('update providers set name = ' + re.body.name + ', email = ' + req.body.email
-			+ ' where provider_id = ' + re.params.id, function(err, result) {
+	connection.query('update providers set name = ' + connection.escape(req.body.name) + ', email = ' + connection.escape(req.body.email)
+			+ ' where provider_id = ' + connection.escape(req.params.id), function(err, result) {
 		if (err)
 			throw err;
 		if (result)
 			res.type('application/json');
 		res.send([ {
-			"provider data updated" : 1
+			"msg" : "Provider details updated."
 		} ]);
 	});
 });
@@ -111,7 +150,7 @@ app.delete('/api/providers/:id', function(req, res) {
 		if (result)
 			res.type('application/json');
 		res.send([ {
-			"provider deleted" : 1
+			"msg" : "Provider deleted."
 		} ]);
 	});
 });
--- server.js
+++ server.js
@@ -1,4 +1,23 @@
 var http = require("http");
+var async = require('async');
+
+var square = function (num,doneCallback){
+	console.log(num*num);
+	// Nothing went wrong, so callback with a null error.
+	  return doneCallback(null);	
+};
+
+//Square each number in the array [1, 2, 3, 4]. 
+
+
+/* The first argument is an array of items to be iterated over.
+ * The second argument is the iterator function – in this case, that's square. With async.each the iterator takes 
+ * two arguments - the current item and a done callback. The iterator can do what it likes to the item 
+ * so long as it calls the done callback at some point, with or without an error.
+ */
+async.each([1,2,3,4],square,function(err){
+	console.log('Finished!');
+});
 
 http.createServer(function(request, response) {
 	response.writeHead(200, {
